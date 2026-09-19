29c7b0ed90edd7418417734744f675cb02daed68
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-09-19 18:26:53 +0300

    Update HallSensor.c

--- main/hall/HallSensor.c
+++ main/hall/HallSensor.c
@@ -2,10 +2,11 @@
 #include "esp_idf_version.h"
 //#include "esp_log.h"
 #if (ESP_IDF_VERSION_MAJOR < 6)
+#define OLD_DRIVER
 #define CONFIG_ADC_SUPPRESS_DEPRECATE_WARN 1
 #include "driver/adc.h"
 #else
-#include "esp_adc/adc_oneshot.h" //dont work
+#include "esp_adc/adc_oneshot.h"
 #endif
 //https://github.com/espressif/esp-idf/blob/12f36a021f511cd4de41d3fffff146c5336ac1e7/docs/en/migration-guides/release-5.x/5.0/peripherals.rst#L59
 //#include "esp_private/adc_share_hw_ctrl.h" //adc_power_acquire
@@ -14,7 +15,7 @@
 //#include "hal/adc_hal_common.h"
 __unused static const char* TAG = "ADC";
 extern portMUX_TYPE rtc_spinlock;
-static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+//static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 // duplicate this because it is static in adc_legacy.h
 static esp_err_t adc_hal_convert(adc_unit_t adc_n, int channel, int *out_raw)
 {
@@ -48,32 +49,32 @@ static int adc_hal_hall_result(void)
 int hall_sensor_read(void)    //hall sensor without LNA //_get_value()
 {
     //adc_power_acquire();
-    portENTER_CRITICAL(&rtc_spinlock); //ADC_ENTER_CRITICAL();
+    //portENTER_CRITICAL(&rtc_spinlock); //ADC_ENTER_CRITICAL();
     //adc_ll_amp_disable(); // disable other peripherals.           //moved to init
     //RTCIO.hall_sens.xpd_hall = 1;  //adc_hal_hall_enable();  //moved to init
     //adc_ll_set_controller(ADC_UNIT_1, ADC_LL_CTRL_RTC);  // adc_hal_set_controller(ADC_UNIT_1, ADC_CTRL_RTC); //moved to init
     int hall_value = adc_hal_hall_result();
     //RTCIO.hall_sens.xpd_hall = 0;  // adc_hal_hall_disable();
-    portEXIT_CRITICAL(&rtc_spinlock); // ADC_EXIT_CRITICAL();
+    //portEXIT_CRITICAL(&rtc_spinlock); // ADC_EXIT_CRITICAL();
     //adc_power_release();
     return hall_value;
 }
 
 void hall_sensor_init(void) {
-#if (ESP_IDF_VERSION_MAJOR < 6)
-    adc1_config_width(ADC_WIDTH_BIT_12);
+#ifdef OLD_DRIVER
+    adc1_config_width(ADC_WIDTH_BIT_12); 
     adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_0);
     adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_0);
 #else
-	adc_oneshot_unit_handle_t adc1_handle;
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&(adc_oneshot_unit_init_cfg_t) { ADC_UNIT_1 }, &adc1_handle));
+	adc_oneshot_unit_handle_t h_adc1;
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&(adc_oneshot_unit_init_cfg_t) { ADC_UNIT_1 }, &h_adc1));
     adc_oneshot_chan_cfg_t config = { ADC_ATTEN_DB_0, ADC_BITWIDTH_12 };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &config));
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &config));
-    adc_ll_amp_disable();
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(h_adc1, ADC_CHANNEL_0, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(h_adc1, ADC_CHANNEL_3, &config));
+#endif
+	adc_ll_amp_disable();
     adc_ll_hall_enable();    // adc_hal_hall_enable(); //RTCIO.hall_sens.xpd_hall = 1;
     adc_ll_set_controller(ADC_UNIT_1, ADC_LL_CTRL_RTC);     // adc_hal_set_controller(ADC_UNIT_1, ADC_CTRL_RTC);
-#endif
 }
 
 void hall_sensor_deinit(void) {
